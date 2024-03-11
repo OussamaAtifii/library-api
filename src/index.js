ad9bcccc757f071a58cec108369b38f6b701db0c
OussamaAtifii <oussamaati03@gmail.com>
2024-03-12 00:20:52 +0100

    Add author route and connect to mongodb

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express, { json } from 'express'
+import mongoose from 'mongoose'
 import logger from 'morgan'
 import dotenv from 'dotenv'
 
 import { booksRoute } from './routes/bookRoute.js'
+import { authorRouter } from './routes/authorRoute.js'
 
 dotenv.config()
 
@@ -12,9 +14,14 @@ app.disable('x-powered-by')
 app.use(json())
 app.use(logger('dev'))
 app.use('/api/books', booksRoute)
+app.use('/api/authors', authorRouter)
 
 const PORT = process.env.PORT ?? 3000
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`Server listening on port http://localhost:${PORT}`)
 })
+
+mongoose.connect(process.env.DB_URL)
+  .then(() => console.log('Database connected'))
+  .catch((error) => console.log(`Error while connecting to database: ${error}`))
